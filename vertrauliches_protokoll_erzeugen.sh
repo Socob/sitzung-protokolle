f8db649999b91fd71199f4afc5c7118edf94ee03
Simon May <simon.may@uni-muenster.de>
2018-01-24 17:20:01 +0100

    Zusätzliche Informationen im Skript ausgeben

--- vertrauliches_protokoll_erzeugen.sh
+++ vertrauliches_protokoll_erzeugen.sh
@@ -2,13 +2,18 @@
 # Alle Dateien mit Namen der Form „*-*-*_protokoll*.tex“ mit latexmk 
 # (eingestellt auf pdflatex) kompilieren. Dabei wird die nicht öffentliche
 # Version durch Setzen von \protokollprivatetrue erzeugt.
+echo "Kompiliere TeX-Dateien im aktuellen Verzeichnis"
+echo "($(pwd))"
+echo
 find -maxdepth 1 -type f -name '*-*-*_protokoll*.tex' -execdir sh -c '
+	echo "Kompiliere {}"
 	tex_prefix="\newif\ifprotokollprivate\protokollprivatetrue"
 	file="$(basename -- "$0" .tex)"
 	latexmk -pdf -silent --jobname="$file"_vertraulich \
 		-pdflatex="pdflatex %O \"$tex_prefix\input{$file}\"" \
 		"$file"
+	echo
 ' '{}' ';'
 # Hilfsdateien entfernen
-rm *.aux *.log *.out *.toc *.fls *.fdb_latexmk *.synctex.gz
+rm -f *.aux *.log *.out *.toc *.fls *.fdb_latexmk *.synctex.gz
 
