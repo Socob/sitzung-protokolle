02c7f325ebb8fef8d98c410a8f965639b04d0347
a_neuw01 <a_neuw01@wwu.de>
2022-06-07 11:34:06 +0000

    Update on Overleaf.

---
+++ vertrauliches_protokoll_erzeugen.bat
@@ -0,0 +1,16 @@
+:: Alle Dateien mit Namen der Form „*-*-*_protokoll*.tex“ mit latexmk 
+:: (eingestellt auf pdflatex) kompilieren. Dabei wird die nicht öffentliche
+:: Version durch Setzen von \protokollprivatetrue erzeugt.
+setlocal
+pushd %~dp0
+
+for %%f in (*-*-*_protokoll*.tex) do (
+	latexmk -pdf -silent --jobname="%%~nf_vertraulich" ^
+		-pdflatex="pdflatex %%O \newif\ifprotokollprivate\protokollprivatetrue\input{%%~nf}" ^
+		"%%~nf"
+)
+:: Hilfsdateien entfernen
+del *.aux *.log *.out *.toc *.fls *.fdb_latexmk *.synctex.gz
+
+popd
+
